91cd10b62d78f38315cd5c513258786ebc16953a
ryuryu1090122 <ryuryu1090122@gmail.com>
2023-07-30 19:46:07 +0900

    add Commands

--- main.ts
+++ main.ts
@@ -1,79 +1,112 @@
+import { context } from 'esbuild';
 import {
 	App,
 	Editor,
+	EditorTransaction,
 	MarkdownView,
 	Modal,
 	Notice,
 	Plugin, 
 	PluginSettingTab,
-	Setting
+	Setting,
+	editorEditorField
 } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+import {} from '@codemirror/state'
+import {} from '@codemirror/view'
+
+interface MathToolsSettings {
+	enableAddMathBlockCommand: boolean;
+	enableAddEquationBlockCommand: boolean;
+	enableAddAlignBlockCommand: boolean;
+	enableAddParenthesesCommand: boolean;
+	enableAddFracCommand: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MathToolsSettings = {
+	enableAddMathBlockCommand: true,
+	enableAddEquationBlockCommand: true,
+	enableAddAlignBlockCommand: true,
+	enableAddParenthesesCommand: true,
+	enableAddFracCommand: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MathToolsPlugin extends Plugin {
+	settings: MathToolsSettings;
 
 	async onload() {
 		await this.loadSettings();
+		
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// incert $$ $$
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+			id: 'add-math-block',
+			name: 'Add math block',
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				if (this.settings.enableAddMathBlockCommand) {
 					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
+						editorEditorField
+					}
+					return true;
+				}
+			}
+		});
+
+		this.addCommand({
+			id: 'add-equation-block',
+			name: 'Add equation block',
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				if (this.settings.enableAddEquationBlockCommand) {
+					if (!checking) {
+
+					}
+					return true;
+				}
+			}
+		});
+
+		this.addCommand({
+			id: 'add-align-block',
+			name: 'Add multiple equations block',
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				if (this.settings.enableAddAlignBlockCommand) {
+					if (!checking) {
+
+					}
+					return true;
+				}
+			}
+		});
+
+		this.addCommand({
+			id: 'add-parentheses-block',
+			name: 'Add parentheses block',
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				if (this.settings.enableAddParenthesesCommand) {
+					if (!checking) {
+
+					}
+					return true;
+				}
+			}
+		});
+
+		this.addCommand({
+			id: 'add-frac',
+			name: 'Add frac',
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				if (this.settings.enableAddFracCommand) {
+					if (!checking) {
+
+					}
 					return true;
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new MathToolsSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -114,28 +147,65 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class MathToolsSettingTab extends PluginSettingTab {
+	plugin: MathToolsPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MathToolsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Math tools for Obsidian - Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('$$ ... $$')
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.enableAddMathBlockCommand)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+					this.plugin.settings.enableAddMathBlockCommand = value;
+					await this.plugin.saveData(this.plugin.settings);
+					this.display();
 				}));
+
+		new Setting(containerEl)
+			.setName('begin{equation} ... end{equation}')
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.enableAddEquationBlockCommand)
+				.onChange(async (value) => {
+					this.plugin.settings.enableAddEquationBlockCommand = value;
+					await this.plugin.saveData(this.plugin.settings);
+				}));
+
+		new Setting(containerEl)
+			.setName('begin{align} ... end{align}')
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.enableAddAlignBlockCommand)
+				.onChange(async (value) => {
+					this.plugin.settings.enableAddAlignBlockCommand = value;
+					await this.plugin.saveData(this.plugin.settings);
+				}));
+
+		new Setting(containerEl)
+			.setName('\\right( ... \\left)')
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.enableAddParenthesesCommand)
+				.onChange(async (value) => {
+					this.plugin.settings.enableAddParenthesesCommand = value;
+					await this.plugin.saveData(this.plugin.settings);
+				}));
+
+		new Setting(containerEl)
+			.setName('\\frac{}{}')
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.enableAddFracCommand)
+				.onChange(async (value) => {
+					this.plugin.settings.enableAddFracCommand = value;
+					await this.plugin.saveData(this.plugin.settings);
+				}));
+
 	}
 }
